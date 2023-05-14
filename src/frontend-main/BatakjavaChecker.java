e9f14a77a5771235719aa8f31f057daf0f1cb263
Lubis <luthfanlubis@gmail.com>
2023-05-14 17:40:15 +0900

    Set up frontend to process input

---
+++ src/frontend-main/BatakjavaChecker.java
@@ -0,0 +1,20 @@
+package org.extendj;
+
+import org.extendj.ast.BatakjavaFrontend;
+import org.extendj.ast.Program;
+
+public class BatakjavaChecker extends BatakjavaFrontend {
+
+  public static void main(String[] args) {
+    int exitCode = new BatakjavaChecker().run(args);
+    if (exitCode != 0) {
+      System.exit(exitCode);
+    }
+  }
+
+  public BatakjavaChecker() { super("Batakjava Checker", "0.1"); }
+
+  public int run(String args[]) {
+    return run(args, Program.defaultBytecodeReader(), Program.defaultJavaParser());
+  }
+}
