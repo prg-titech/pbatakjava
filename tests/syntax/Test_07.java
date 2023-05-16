9af9b71aaed1959edf08ef16d7bb7076de353995
Lubis <luthfanlubis@gmail.com>
2023-05-16 17:44:13 +0900

    Updated tests

--- tests/syntax/Test_07.java
+++ tests/syntax/Test_07.java
@@ -1,8 +1,8 @@
 public class Test_08 ver 1 {
   public static void main(String[] args) {
-    new Object().>V<m();
-    super.>V<m();
-    Object.>V<m();
-    Object.super.>V<m(); 
+    new Object().«V»m();
+    super.«V»m();
+    Object.«V»m();
+    Object.super.«V»m(); 
   }
 }
