78f0bd5ca18b98888cb82bc3649921896edffaa5
Lubis <luthfanlubis@gmail.com>
2023-05-17 04:46:04 +0900

    Finished connected components calculation

--- src/frontend-main/DependencyChecker.java
+++ src/frontend-main/DependencyChecker.java
@@ -13,8 +13,25 @@ public class DependencyChecker {
     g.addEdge(3, 3);
     // g.printGraph();
 
-    g.computeComponents();
-    System.out.println(g.components);
+    System.out.println(g.getComponents());
+
+    Graph g2 = new Graph(8);
+    g2.addEdge(0,1);
+    g2.addEdge(1,2);
+    g2.addEdge(2,0);
+    g2.addEdge(3,1);
+    g2.addEdge(3,2);
+    g2.addEdge(3,4);
+    g2.addEdge(4,3);
+    g2.addEdge(4,5);
+    g2.addEdge(5,2);
+    g2.addEdge(5,6);
+    g2.addEdge(6,5);
+    g2.addEdge(7,4);
+    g2.addEdge(7,6);
+    g2.addEdge(7,7);
+
+    System.out.println(g2.getComponents());
   }
 
 }
