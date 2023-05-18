503e1456a2f6dc371d64630709c53a5fc3c44aee
Lubis <luthfanlubis@gmail.com>
2023-05-18 18:28:31 +0900

    Built graph, adjacency matrix from the input source code

--- src/frontend-main/DependencyChecker.java
+++ src/frontend-main/DependencyChecker.java
@@ -1,37 +1,21 @@
 package org.batakjava;
 
-import org.extendj.ast.Graph;
+import org.extendj.ast.DependencyFrontend;
+import org.extendj.ast.Program;
 
-public class DependencyChecker {
+public class DependencyChecker extends DependencyFrontend {
 
   public static void main(String[] args) {
-    Graph g = new Graph(4);
-    g.addEdge(0, 1);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(3, 2);
-    g.addEdge(3, 3);
-    // g.printGraph();
+    int exitCode = new DependencyChecker().run(args);
+    if (exitCode != 0) {
+      System.exit(exitCode);
+    }
+  }
 
-    System.out.println(g.getComponents());
+  public DependencyChecker() { super("BatakJava Dependency Checker", "0.0.1"); }
 
-    Graph g2 = new Graph(8);
-    g2.addEdge(0,1);
-    g2.addEdge(1,2);
-    g2.addEdge(2,0);
-    g2.addEdge(3,1);
-    g2.addEdge(3,2);
-    g2.addEdge(3,4);
-    g2.addEdge(4,3);
-    g2.addEdge(4,5);
-    g2.addEdge(5,2);
-    g2.addEdge(5,6);
-    g2.addEdge(6,5);
-    g2.addEdge(7,4);
-    g2.addEdge(7,6);
-    g2.addEdge(7,7);
-
-    System.out.println(g2.getComponents());
+  public int run(String args[]) {
+    return run(args, Program.defaultBytecodeReader(), Program.defaultJavaParser());
   }
 
 }
