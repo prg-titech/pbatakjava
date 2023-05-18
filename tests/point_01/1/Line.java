9e2dd17392372982aba875274092094773d29c0f
Lubis <luthfanlubis@gmail.com>
2023-05-18 18:32:38 +0900

    Modified tests with the new syntax

--- tests/point_01/1/Line.java
+++ tests/point_01/1/Line.java
@@ -1,14 +1,14 @@
-class Line ver 1 >V,W< {
+class Line ver 1 «V,W» {
   Point#V# p1;
   Point#W# p2;
   Line(Point#V# p1, Point#W# p2) {
     this.p1 = p1;
     this.p2 = p2;
   }
-  >Y< Line>V,Y< update(Point#Y# p) {
-    return new Line>V,Y<(p1, p);
+  «Y» Line«V,Y» update(Point#Y# p) {
+    return new Line«V,Y»(p1, p);
   }
-  >Y< void update() {
-    this.p1 = new Point#V#>Y<(new Color());
+  «Y» void update() {
+    this.p1 = new Point#V#«Y»(new Color());
   }
 }
--- tests/point_01/1/Test.java
+++ tests/point_01/1/Test.java
@@ -1,7 +1,7 @@
 public class Test ver 1 {
   void main(String[] args) {
-    Line l2 = new Line>2,1<(
-      new Point>2<(new Color()), new Point(true));
-    l2.>2<update(new Point>v1<(new Color()));
+    Line l2 = new Line«2,1»(
+      new Point«2»(new Color()), new Point(true));
+    l2.«2»update(new Point«1»(new Color()));
   }
 }
