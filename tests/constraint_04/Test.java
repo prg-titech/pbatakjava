661e70786f508d408e9f7ca25b0142f9d4ee6f9b
Lubis <luthfanlubis@gmail.com>
2023-06-09 16:34:18 +0900

    Constraints added to instantiation's arguments

--- tests/constraint_04/Test.java
+++ tests/constraint_04/Test.java
@@ -1,6 +1,6 @@
 public class Test ver 1 {
   public static void main(String[] args) {
     Line l1 = new Line«1,2»(new Point(), new Point());
-    Line l2 = new Line«2,2»(new Point(), new Point());  // should fail
+    // Line l2 = new Line«2,2»(new Point(), new Point());
   }
 }
