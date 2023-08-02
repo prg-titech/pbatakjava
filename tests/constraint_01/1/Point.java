e2fcc2a3104a2c29e3539f959010d0c7397c4330
Lubis <luthfanlubis@gmail.com>
2023-08-02 22:22:44 +0900

    Base example

--- tests/constraint_01/1/Point.java
+++ tests/constraint_01/1/Point.java
@@ -5,3 +5,18 @@ public class Point ver 1 {
     this.x = x; this.y = y;
   }
 }
+
+/**
+ * public interface Point {
+ * 
+ * }
+ * 
+ * public class Point_v1 implements Point {
+ *   public int x;
+ *   public int y;
+ *   public Point_v1(int x, int y) {
+ *     this.x = x;
+ *     this.y = y;
+ *   }
+ * }
+ */
--- tests/constraint_01/2/Point.java
+++ tests/constraint_01/2/Point.java
@@ -5,3 +5,18 @@ public class Point ver 2 {
     this.x = x; this.y = y;
   }
 }
+
+/**
+ * public interface Point {
+ * 
+ * }
+ * 
+ * public class Point_v2 implements Point {
+ *   public int x;
+ *   public int y;
+ *   public Point_v2(int x, int y) {
+ *     this.x = x;
+ *     this.y = y;
+ *   }
+ * }
+ */
