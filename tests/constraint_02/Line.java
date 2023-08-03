4783f78b3a33d0ba92fd2cfe633f8653fff15ce2
Lubis <luthfanlubis@gmail.com>
2023-08-03 15:03:32 +0900

    Base compilation for version variables

--- tests/constraint_02/Line.java
+++ tests/constraint_02/Line.java
@@ -5,3 +5,35 @@ public class Line ver 1 «V,W» {
     this.p1 = p1; this.p2 = p2;
   }
 }
+
+/**
+ * public interface Line {
+ * 
+ * }
+ * 
+ * public class Line_v1 implements Line {
+ *   public Point_Factory V;
+ *   public Point_Factory W;
+ *   public Point p1;
+ *   public Point p2;
+ *   public Line_v1(Point_Factory V, Point_Factory W
+ *                  Point p1, Point p2) {
+ *     this.V = V;
+ *     this.W = W;
+ *     this.p1 = p1;
+ *     this.p2 = p2;
+ *   }
+ * }
+ * 
+ * public interface Line_Factory {
+ *   public Line make(Point_Factory V, Point_Factory W,
+ *                    Point p1, Point p2);
+ * }
+ * 
+ * public class Line_v1_Factory implements Line_Factory {
+ *   public Line make(Point_Factory V, Point_Factory W,
+ *                    Point p1, Point p2) {
+ *     return new Line_v1(V, W, p1, p2);
+ *   }
+ * }
+ */
--- tests/constraint_02/Point.java
+++ tests/constraint_02/Point.java
@@ -1,3 +1,32 @@
 public class Point ver 1 {
-
+  private int x;
+  private int y;
+  public Point(int x, int y) {
+    this.x = x; this.y = y;
+  }
 }
+
+/**
+ * public interface Point {
+ * 
+ * }
+ * 
+ * public class Point_v1 implements Point {
+ *   public int x;
+ *   public int y;
+ *   public Point_v1(int x, int y) {
+ *     this.x = x;
+ *     this.y = y;
+ *   }
+ * }
+ * 
+ * public interface Point_Factory {
+ *   public Point make(int x, int y);
+ * }
+ * 
+ * public class Point_v1_Factory implements Point_Factory {
+ *   public Point make(int x, int y) {
+ *     return new Point_v1(x, y);
+ *   }
+ * }
+ */
