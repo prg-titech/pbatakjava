7cf4e165be321d1791e04e1744263910c6e8782f
Lubis <luthfanlubis@gmail.com>
2023-08-14 22:40:44 +0900

    Add examples with method invoc. and failed method invoc.

--- tests/constraint_04/build.sh
+++ tests/constraint_04/build.sh
@@ -1,3 +1,5 @@
 #/bin/sh
 
+rm -rf tests/constraint_04/class
+mkdir tests/constraint_04/class
 java -jar pbatakjava.jar tests/constraint_04/1/*.java tests/constraint_04/2/*.java tests/constraint_04/*.java
--- tests/error_01/Test.java
+++ tests/error_01/Test.java
@@ -1,7 +1,7 @@
 public class Test ver 1 {
   public static void main(String[] args) {
-    Point p1 = new Point(1,1);
+    Point p1 = new Point(2,2);
     p1.m();
-    Line l1 = new Line«1,2»(p1, new Point(2,2));
+    Line l1 = new Line«2,2»(p1, new Point(2,2));
   }
 }
