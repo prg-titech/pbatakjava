9ce984c8bcdc8a33ed068f0438c9cd77c560b643
Lubis <luthfanlubis@gmail.com>
2023-08-16 13:44:57 +0900

    Add constructor error example

--- tests/constraint_05/build.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-java -jar pbatakjava.jar tests/constraint_05/1/*.java tests/constraint_05/2/*.java tests/constraint_05/*.java
---
+++ tests/error_02/Test.java
@@ -0,0 +1,5 @@
+public class Test ver 1 {
+  public static void main(String[] args) {
+    Line l1 = new Line«1,1»(new Color(), new Point());
+  }
+}
