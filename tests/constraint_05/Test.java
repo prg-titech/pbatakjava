3f06128e1ac95bb4ee503b03e6f3f028a8d029e3
Lubis <luthfanlubis@gmail.com>
2023-08-23 02:19:58 +0900

    Example for method compilation

---
+++ tests/constraint_05/Test.java
@@ -0,0 +1,6 @@
+public class Test ver 1 {
+  public static void main(String[] args) {
+    Line l1 = new Line«1,2»(new Point(1,1), new Point(2,2));
+    Line l2 = l1.«2»update(new Point(2,2));
+  }
+}
