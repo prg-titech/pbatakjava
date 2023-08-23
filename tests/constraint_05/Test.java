3b03e6d7814349db02472729d99907c8fed57cc5
Lubis <luthfanlubis@gmail.com>
2023-08-23 09:40:59 +0900

    Add an error test

--- tests/constraint_05/Test.java
+++ tests/constraint_05/Test.java
@@ -4,3 +4,12 @@ public class Test ver 1 {
     Line l2 = l1.«2»update(new Point(2,2));
   }
 }
+
+/**
+ * public class Test_v1 implements Test {
+ *  public static void main(String[] args) {
+ *    Line_v1 l1 = new Line_v1(new Point_v1_Factory(), new Point_v2_Factory(), new Point_v1(1, 1), new Point_v2(2, 2));
+ *    Line_v1 l2 = l1.update(new Point_v2_Factory(), new Point_v2(2, 2));
+ *  }
+ * } 
+ */
