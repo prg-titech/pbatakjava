0d52071d94cc535c5843e88914cd9e8b702450d2
Lubis <luthfanlubis@gmail.com>
2023-08-26 20:02:39 +0900

    Rename a test

--- tests/constraint_06/2/Point.java
+++ tests/constraint_06/2/Point.java
@@ -15,3 +15,50 @@ public class Point ver 2 {
     return new Point#V#(y, x);
   }
 }
+
+
+
+/**
+ * public interface Point_Factory {
+ *   public Point make(int x, int y);
+ *   public Point inverse(Point p);
+ * }
+ * 
+ * public class Point_v2_Factory implements Point_Factory {
+ *   public Point make(int x, int y) { return new Point_v2(x, y); }
+ *   public Point inverse(Point p) { return ((Point_v2) p).inverse(this); }
+ * }
+ * 
+ * public class Point_v1_Factory implements Point_Factory {
+ *   public Point make(int x, int y) { return new Point_v1(x, y); }
+ *   public Point inverse(Point p) { return nullptr; }
+ * }
+ * 
+ * up won't work because we might have multiple arguments
+ * 
+ * down is the better option, although this will make code bloats
+ * 
+ * public interface Point {
+ *   public Point inverse(Point_Factory V);
+ * }
+ * 
+ * public class Point_v2 implements Point {
+ *   public int x;
+ *   public int y;
+ *   public Point_v2(int x, int y) {
+ *     this.x = x;
+ *     this.y = y;
+ *   }
+ *   public Point inverse(Point_Factory V) { return V.make(y, x); }
+ * }
+ * 
+ * public class Point_v1 implements Point {
+ *   public int x;
+ *   public int y;
+ *   public Point_v2(int x, int y) {
+ *     this.x = x;
+ *     this.y = y;
+ *   }
+ *   public Point inverse(Point_Factory V) { return nullptr; }
+ * }
+ */
---
+++ tests/constraint_06/Test.java
@@ -0,0 +1,13 @@
+public class Test ver 1 {
+  public static void main(String[] args) {
+    
+  }
+}
+
+/**
+ * public class Test_v1 implements Test {
+ *  public static void main(String[] args) {
+ * 
+ *  }
+ * } 
+ */
