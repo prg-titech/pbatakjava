ee6520f38dd0b96e5a2848318020161483b63f59
Lubis <luthfanlubis@gmail.com>
2023-09-04 13:08:46 +0900

    Example for version consistency

---
+++ tests/error_06/1/Point.java
@@ -0,0 +1,11 @@
+public class Point ver 1 {
+  public int x;
+  public int y;
+  public Point(int x, int y) {
+    this.x = x; this.y = y;
+  }
+
+  public void update(int newX) {
+    this.x = newX;
+  }
+}
---
+++ tests/error_06/2/Point.java
@@ -0,0 +1,11 @@
+public class Point ver 2 {
+  public int x;
+  public int y;
+  public Point(int x, int y) {
+    this.x = x; this.y = y;
+  }
+
+  public void modify(int newX) {
+    this.x = newX;
+  }
+}
