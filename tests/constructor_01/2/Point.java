841e3b50b4ac66dea2a0debf2fea7e6ff0a03168
Lubis <luthfanlubis@gmail.com>
2023-09-22 07:53:45 +0900

    Add constructor change test

--- tests/constructor_01/2/Point.java
+++ tests/constructor_01/2/Point.java
@@ -1,4 +1,4 @@
-public class Point ver 1 {
+public class Point ver 2 {
   public int x;
   public int y;
   public int z;
@@ -8,3 +8,33 @@ public class Point ver 1 {
     this.z = z;
   }
 }
+
+/**
+ * 
+ * public interface Point {}
+ * 
+ * public class Point_v2 implements Point {
+ *   public int x;
+ *   public int y;
+ *   public int z;
+ *   public Point_v2(int x, int y, int z) {
+ *     this.x = x ; this.y = y; this.z = z;
+ *   }
+ * }
+ * 
+ * // gather everything
+ * public interface Point_Factory {
+ *   public Point make(int x, int y);
+ *   public Point make(int x, int y, int z);
+ * }
+ * 
+ * public class Point_v2_Factory implements Point_Factory {
+ *   public Point make(int x, int y) {
+ *     return null;
+ *   }
+ *   public Point make(int x, int y, int z) {
+ *     return new Point_v2(x, y, z);
+ *   }
+ * }
+ * 
+ */
