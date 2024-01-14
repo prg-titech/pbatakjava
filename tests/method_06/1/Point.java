f96d02fd2b4745b77a0817d8bbf2b8bf7ca99e3c
Lubis <luthfanlubis@gmail.com>
2024-01-14 20:45:12 +0900

    Update tests

--- tests/method_06/1/Point.java
+++ tests/method_06/1/Point.java
@@ -1,10 +1,10 @@
-public class Point ver 2 {
+public class Point ver 1 {
   public int x;
   public int y;
   public Point(int x, int y) {
     this.x = x; this.y = y;
   }
-  public «V» Point#V# inverse() {
-    return new Point#V#(y, x);
+  public int getX() {
+    return this.x;
   }
 }
---
+++ tests/method_06/3/Point.java
@@ -0,0 +1,10 @@
+public class Point ver 2 {
+  public float x;
+  public float y;
+  public Point(int x, int y) {
+    this.x = x; this.y = y;
+  }
+  public float getX() {
+    return this.x;
+  }
+}
